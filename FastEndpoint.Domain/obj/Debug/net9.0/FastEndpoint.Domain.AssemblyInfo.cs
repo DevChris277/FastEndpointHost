e72d8919cac47d6ee5caf0fea1c0a18be20bb1cc
christopher <chris.needham277@gmail.com>
2025-05-31 11:29:06 +0100

    Remove cached files and folders

--- FastEndpoint.Domain/obj/Debug/net9.0/FastEndpoint.Domain.AssemblyInfo.cs
+++ FastEndpoint.Domain/obj/Debug/net9.0/FastEndpoint.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastEndpoint.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c87f385e07130c362385496dd631bb09efc43b7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e56a5c7bd3688cd6212dd13661118ebf5578c67b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastEndpoint.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastEndpoint.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
